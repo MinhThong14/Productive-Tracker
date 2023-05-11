e60bd476684cda4daa996d16e4f8ffaa3ad2e411
MinhThong14 <achilles.mai14@gmail.com>
2023-05-10 22:27:19 -0400

    Added addActivity function

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,32 @@ import {useEffect, useState} from "react";
 function App() {
   const [activities, setActivities] = useState([]);
 
+  const addActivity = async (event) => {
+    event.preventDefault();
+    
+    const newActivity = {
+      name: event.target.activity.value,
+      time: event.target.time.value,
+    };
+    
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/activity`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newActivity),
+    });
+  
+    event.target.activity.value = ""; // sets input empty after clicking submit
+    event.target.time.value = ""; // sets input empty after clicking submit
+    window.location.reload(); // reloads the window after sending request
+  };
+
   return (
     <div className="app">
       <header className="app-header">
         <h1>Productivity Tracker</h1>
-        <form>
+        <form onSubmit={addActivity}>
           <div>
             <label htmlFor="activity">Activity:</label>
             <input
