eb269fbe4817a91d738d588b30a0789194e87e48
MinhThong14 <achilles.mai14@gmail.com>
2023-05-10 23:08:47 -0400

    Added cors to server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -13,6 +14,9 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/todoapiDB";
     /* Telling the application to use the express.json() middleware. This middleware will parse the body of
 any request that has a Content-Type of application/json. */
 app.use(express.json());
+
+/* Allowing the frontend to access the backend. */
+app.use(cors());
 
 /* This is a route handler. It is listening for a GET request to the root route of the application.
 When it receives a request, it will send back a response with the string "Hello World!". */
