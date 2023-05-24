dd3a5a078696c9b6aa5a4e3edb31eae6eaf82b47
MinhThong14 <achilles.mai14@gmail.com>
2023-05-23 22:54:29 -0400

    Added client tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
   const [activities, setActivities] = useState([]);
+
   const token = localStorage.getItem("token");
 
- /* Fetching the data from the backend and setting the state of activities to the data. */
- useEffect(() => {
+  /* Fetching the data from the backend and setting the state of activities to the data. */
+  useEffect(() => {
     const fetchData = async () => {
       const result = await fetch(
         `${process.env.REACT_APP_BACKEND_URL}/activities`,
@@ -20,16 +22,21 @@ function App() {
       setActivities(data);
     };
     fetchData();
-  }, []);
+  }, [token]);
 
+  /**
+   * When the user clicks the submit button, the function will prevent the default action of the form,
+   * create a new activity object with the name and time values from the form, and then send a POST
+   * request to the backend to create a new activity
+   */
   const addActivity = async (event) => {
     event.preventDefault();
-    
+
     const newActivity = {
       name: event.target.activity.value,
       time: event.target.time.value,
     };
-    
+
     await fetch(`${process.env.REACT_APP_BACKEND_URL}/activity`, {
       method: "POST",
       headers: {
@@ -38,16 +45,16 @@ function App() {
       },
       body: JSON.stringify(newActivity),
     });
-  
-    event.target.activity.value = ""; // sets input empty after clicking submit
-    event.target.time.value = ""; // sets input empty after clicking submit
-    window.location.reload(); // reloads the window after sending request
+
+    event.target.activity.value = "";
+    event.target.time.value = "";
+    window.location.reload();
   };
 
   return (
     <div className="app">
       <header className="app-header">
-        <h1>Productivity Tracker</h1>
+        <h1 data-testid="app-header-heading">Productivity Tracker</h1>
         <form onSubmit={addActivity}>
           <div>
             <label htmlFor="activity">Activity:</label>
@@ -56,31 +63,41 @@ function App() {
               id="activity"
               name="activity"
               autoComplete="off"
+              data-testid={"activity-name-input"}
             />
           </div>
           <div>
             <label htmlFor="time">Time Taken:</label>
-            <input type="text" id="time" name="time" autoComplete="off" />
+            <input
+              type="text"
+              id="time"
+              name="time"
+              autoComplete="off"
+              data-testid="activity-duration-input"
+            />
           </div>
-          <button type="submit">Add</button>
+          <button type="submit" data-testid="activity-add-button">
+            Add
+          </button>
         </form>
       </header>
       <main className="app-main">
         <h2>Today</h2>
+
         {activities && activities.length > 0 ? (
-        <ol>
-          {activities.map((activity) => (
-            <li key={activity._id}>
-              {activity.name} - {activity.time}
-            </li>
-          ))}
-        </ol>
+          <ol>
+            {activities.map((activity) => (
+              <li key={activity._id}>
+                {activity.name} - {activity.time}
+              </li>
+            ))}
+          </ol>
         ) : (
           <p>No activities yet</p>
         )}
-        </main>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
